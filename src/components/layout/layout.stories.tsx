a1a4a532ca7709afe53764d6fe0c319a06c7e8e9
carlonomes <nomes.carlo@gmail.com>
2020-12-23 17:11:35 +0100

    🎨  add story name to `Box` story

--- src/components/layout/layout.stories.tsx
+++ src/components/layout/layout.stories.tsx
@@ -1,7 +1,9 @@
+import { Story } from '@storybook/react/types-6-0';
 import React from 'react';
 import Box from './Box';
 
 const story = { title: 'Layout' };
 export default story;
 
-export const BoxStory = () => <Box />;
+export const BoxStory: Story = () => <Box />;
+BoxStory.storyName = 'Box';
