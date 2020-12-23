e42f2470a158bccdea4bde2fe9e1b2cca3cef1ed
carlonomes <nomes.carlo@gmail.com>
2020-12-23 16:33:57 +0100

    🎨  change `size` layout functionality to not use default value

--- src/components/layout/css/size.ts
+++ src/components/layout/css/size.ts
@@ -6,21 +6,22 @@ export type SizeProps = {
   height?: string | number;
 };
 
-const DEFAULT_SIZE = '100%';
 function get(key: 'width' | 'height') {
   return (props: SizeProps): string => {
-    if (typeof props[key] === 'number') return `${props[key]}rem`;
-    if (typeof props[key] === 'string') return props[key] as string;
-    if (typeof props.size === 'number') return `${props.size}rem`;
-    if (typeof props.size === 'string') return props.size;
-    return DEFAULT_SIZE;
+    if (typeof props[key] === 'number') return `${key}: ${props[key]}rem`;
+    if (typeof props[key] === 'string') return `${key}: ${props[key]}`;
+
+    if (typeof props.size === 'number') return `${key}: ${props.size}rem`;
+    if (typeof props.size === 'string') return `${key}: ${props.size}`;
+
+    return '';
   };
 }
 
 const size = css<SizeProps>`
   box-sizing: border-box;
-  width: ${get('width')};
-  height: ${get('height')};
+  ${get('width')};
+  ${get('height')};
 `;
 
 export default size;
