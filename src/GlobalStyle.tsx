af40d8195c47b3f375e65a3a2ae257faef6813cb
carlonomes <nomes.carlo@gmail.com>
2021-01-23 14:32:57 +0100

    ⚡️ add additional theming variables

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -17,10 +17,19 @@ const variables = css`
     --colors-danger: #b7403c;
 
     --colors-darken-1: rgba(0, 0, 0, 0.05);
+    --colors-darken-2: rgba(0, 0, 0, 0.1);
+    --colors-darken-3: rgba(0, 0, 0, 0.2);
+    --colors-darken-4: rgba(0, 0, 0, 0.4);
+    --colors-darken-5: rgba(0, 0, 0, 0.8);
 
     --colors-lighten-1: rgba(255, 255, 255, 0.05);
+    --colors-lighten-2: rgba(255, 255, 255, 0.1);
+    --colors-lighten-3: rgba(255, 255, 255, 0.2);
+    --colors-lighten-4: rgba(255, 255, 255, 0.4);
+    --colors-lighten-5: rgba(255, 255, 255, 0.8);
 
     --border-width-default: 0.05rem;
+    --border-width-large: 0.15rem;
     --border-radius-default: 0.25rem;
 
     --spacing-default: 1rem;
