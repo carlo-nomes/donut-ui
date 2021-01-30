19bbca3f57f67db64a8f7b387ebd65a09c9193e2
carlonomes <nomes.carlo@gmail.com>
2021-01-30 17:07:01 +0100

    ✨ add focus within to formgroup

--- src/components/forms/FormGroup.tsx
+++ src/components/forms/FormGroup.tsx
@@ -4,6 +4,10 @@ const FormGroup = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: var(--spacing-default);
+
+  &:focus-within label {
+    color: var(--colors-primary);
+  }
 `;
 
 export default FormGroup;
