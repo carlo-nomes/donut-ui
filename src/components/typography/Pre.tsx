6142cf675a113c47dc22db1cd89b7ee3b05f5754
carlonomes <nomes.carlo@gmail.com>
2021-03-01 09:14:46 +0100

    ✨ add `Pre` component
      - small refactor typography story

---
+++ src/components/typography/Pre.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Pre = styled.pre`
+  width: 100%;
+  border-radius: var(--border-radius-default);
+  margin: 0;
+  padding: var(--spacing-default);
+
+  background-color: var(--colors-gray-2);
+`;
+
+export default Pre;
--- src/components/typography/typography.stories.tsx
+++ src/components/typography/typography.stories.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react';
 import Box, { BoxProps } from '../layout/Box';
 import Code from './Code';
 import { H1, H2, H3, H4, H5 } from './Heading';
+import Pre from './Pre';
 import Text from './Text';
 
 const story = { title: 'General/Typography' };
@@ -27,21 +28,22 @@ export const TextStory: Story = () => (
   <CenterBox>
     <H2>Lorem Ipsum</H2>
     <Text>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem itaque
-      ipsum iusto porro, aliquam explicabo totam provident assumenda laborum,
-      nostrum illum sed illo voluptate voluptates sapiente molestiae ut odit!
-      Modi?
+      <b>Lorem ipsum</b> dolor sit amet consectetur adipisicing elit. Dolorem
+      itaque ipsum iusto porro, aliquam explicabo totam provident assumenda
+      laborum, nostrum illum sed illo voluptate voluptates sapiente molestiae ut
+      odit! Modi?
     </Text>
     <Text>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sed
-      nesciunt doloribus quaerat consectetur quod excepturi sint unde. Sint
+      <i>Lorem ipsum</i> dolor sit amet consectetur adipisicing elit. Officiis
+      sed nesciunt doloribus quaerat consectetur quod excepturi sint unde. Sint
       exercitationem voluptates explicabo, error culpa reiciendis aut qui unde
       obcaecati suscipit?
     </Text>
     <Text>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, fugiat
-      quo quibusdam placeat harum velit voluptas, commodi provident molestiae,
-      in quos corporis non repellat labore cupiditate eum facilis laborum et?
+      <u>Lorem ipsum</u> dolor sit, amet consectetur adipisicing elit. Tenetur,
+      fugiat quo quibusdam placeat harum velit voluptas, commodi provident
+      molestiae, in quos corporis non repellat labore cupiditate eum facilis
+      laborum et?
     </Text>
   </CenterBox>
 );
@@ -49,14 +51,19 @@ TextStory.storyName = 'Text';
 
 export const CodeStory: Story = () => {
   const snippet = `
-  function sum(a, b){
-    return a + b;
-  }
+const calculator = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  divide: (a, b) => a / b,
+  multiply: (a, b) => a * b,
+}
   `;
 
   return (
     <CenterBox>
-      <Code>{snippet}</Code>
+      <Pre>
+        <Code>{snippet}</Code>
+      </Pre>
     </CenterBox>
   );
 };
