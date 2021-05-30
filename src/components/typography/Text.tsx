ba2bb8d810daa116ce0bb16d00689a85788baa0c
carlo.nomes <nomes.carlo@gmail.com>
2021-05-30 23:07:01 +0200

    :zap: small code improvement

--- src/components/typography/Text.tsx
+++ src/components/typography/Text.tsx
@@ -9,9 +9,7 @@ const StyledParagraph = styled.p<MarginProps>`
 
 export type TextProps = MarginProps;
 const Text: FC<TextProps> = ({ m, my, mb, ...other }) => {
-  if (m === undefined && my === undefined && mb === undefined) {
-    mb = 1;
-  }
+  if ([m, my, mb].every((v) => v === undefined)) mb = 1;
 
   return <StyledParagraph m={m} my={my} mb={mb} {...other} />;
 };
