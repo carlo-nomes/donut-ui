45ee14afa472c7e2634034bcf0ffbcf33df52168
carlo.nomes <nomes.carlo@gmail.com>
2023-04-10 18:00:41 +0200

    extra

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,7 +1,13 @@
 import { Box } from '@donut-ui/layout';
+import { H1, H2 } from '@donut-ui/typography';
 
 function App() {
-  return <Box>Demo</Box>;
+  return (
+    <Box>
+      <H1>Donut UI</H1>
+      <H2>Demo Application</H2>
+    </Box>
+  );
 }
 
 export default App;
